f5b409842bcb7acad7f86161331d72da19c12ac7
artoo <artoo@inventati.org>
2020-12-15 13:53:51 +0000

    --

--- www/status.php
+++ www/status.php
@@ -130,9 +130,7 @@
     #$sql_active = "SELECT name from schedules WHERE active = 1;";
     $sql_active = "SELECT *, schedules.id as Sid, schedules.name as Sname, sensors.value as Svalue, sched_sensor.value as Tvalue FROM schedules LEFT JOIN sched_sensor ON schedules.id=sched_sensor.sched_id LEFT JOIN sensors ON sched_sensor.sensor_id=sensors.id WHERE active = 1";
     $result_active = mysqli_query($conn, $sql_active);
-    if (mysqli_num_rows($result_active) == 0) {
-        #echo "No active schedules"; 
-    }
+    
 
     $sql_network = "SELECT name FROM network where value = 1;";
     $phone_home = mysqli_query($conn, $sql_network);
@@ -231,15 +229,16 @@
     
     echo "<table class='ttab'>";
     echo "<tr><center> Active schedule: ";
-    if (is_null(mysqli_fetch_assoc($result_active))){
-	    echo "<b>none</b></center>"; 
-    }else{
+    if (mysqli_num_rows($result_active) == 0) {
+        echo "<b>none</b></center>";
+    }
     while($row = mysqli_fetch_assoc($result_active)) {
 	    $ACTIVE = $row["Sname"];
 	    $TARGET = $row["Tvalue"];
 	    $ROOM = $row["name"];
-	    echo "'<b>" . $ACTIVE . "</b>' and target temperature is <b>" . $TARGET . "</b> in " . $ROOM . "</center>"; 
-	}
+	    echo "'<b>" . $ACTIVE . 
+  	    "</b>' and target temperature is <b>" . $TARGET . 
+	    "</b> in " . $ROOM . "</center>"; 
     }
  
 
